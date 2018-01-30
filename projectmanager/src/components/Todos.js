ea3b67e55aaee2bc1d95b969802f5d7659263a22
Saif <saifahsan92@gmail.com>
2018-01-30 16:47:11 +0600

    This is my first commit

---
+++ projectmanager/src/components/Todos.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import TodoItem from './TodoItems';
+
+class Todos extends Component {
+
+  render() {
+    let todoItems;
+    if (this.props.todos) {
+      todoItems = this.props.todos.map(todo => {
+        return (<TodoItem key={todo.title} todo={todo}/>);
+
+      });
+    }
+    return (<div className="Todos">
+      <h3>
+        Todo List
+      </h3>
+      {todoItems}
+    </div>);
+  }
+}
+
+
+export default Todos;
